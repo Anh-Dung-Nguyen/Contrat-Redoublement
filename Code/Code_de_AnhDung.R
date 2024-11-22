d9219716bc560350b1d8f3ab51bbf6eaf76095f9
Nguyen Anh Dung <nguyenanhdung2807@gmail.com>
2024-11-22 04:33:15 +0100

    Modifie les noms de EC

--- Code/Code_de_AnhDung.R
+++ Code/Code_de_AnhDung.R
@@ -71,17 +71,17 @@ compensation_inter_UE <- function(df, ue_scientifique){
 }
 
 # EC pour S3
-EC_FONDA_S3 <- c("ALG","ANA","INFO","MECA")
+EC_FONDA_S3 <- c("ALG","ANAL","INFO","MECA")
 evaluations_FONDA_S3 <- list(
   ALG = c("DS1", "DS2"),
-  ANA = c("DS1", "DS2"),
+  ANAL = c("DS1", "DS2"),
   INFO = c("DS1", "DS2"),
   MECA = c("DS1", "DS2", "CC")
 )
 
 poids_FONDA_S3 <- list(
   ALG = c(1.5, 1.5),
-  ANA = c(1.5, 1.5),
+  ANAL = c(1.5, 1.5),
   INFO = c(1, 1),
   MECA = c(1, 3, 1)
 )
@@ -90,33 +90,33 @@ ECTS_FONDA_S3 <- c(2, 3, 2, 2)
 
 dfs3_fonda <- generer_notes_automatique("FONDA S3", EC_FONDA_S3, evaluations_FONDA_S3, poids_FONDA_S3, ECTS_FONDA_S3)
 
-EC_EXP_S3 <- c("ACSA","Chimie","Electro","TP_Phys","Thermo")
+EC_EXP_S3 <- c("ACSA","CHIM","ELEC","PHYS","THEN")
 evaluations_EXP_S3 <- list(
   ACSA = c("DS2", "TP"),
-  Chimie = c("DS1", "DS2","TP"),
-  Electro = c("DS2", "CC"),
-  TP_Phys = c("TP"),
-  Thermo = c("DS2")
+  CHIM = c("DS1", "DS2","TP"),
+  ELEC = c("DS2", "CC"),
+  PHYS = c("TP"),
+  THEN = c("DS2")
 )
 
 poids_EXP_S3 <- list(
   ACSA = c(5, 3),
-  Chimie = c(1, 1, 1),
-  Electro = c(2, 1),
-  TP_Phys = c(2, 1),
-  Thermo = c(1)
+  CHIM = c(1, 1, 1),
+  ELEC = c(2, 1),
+  PHYS = c(2, 1),
+  THEN = c(1)
 )
 
 ECTS_EXP_S3 = c(3, 3.5, 1.5, 1.5, 1.5)
 
 dfs3_exp <- generer_notes_automatique("EXP S3", EC_EXP_S3, evaluations_EXP_S3, poids_EXP_S3, ECTS_EXP_S3)
 
-EC_ORT_S3 <- c("ADS","PPI","RIE","Stage","TSE")
+EC_ORT_S3 <- c("ADS","PPI","RIE","STAG","TSE")
 evaluations_ORT_S3 <- list(
   ADS = c(),
   PPI = c(),
   RIE = c(),
-  Stage = c("DS2"),
+  STAG = c("DS2"),
   TSE = c("DS2")
 )
 
@@ -124,7 +124,7 @@ poids_ORT_S3 <- list(
   ADS = c(),
   PPI = c(),
   RIE = c(),
-  Stage = c(1),
+  STAG = c(1),
   TSE = c(1)
 )
 
@@ -132,17 +132,17 @@ ECTS_ORT_S3 = c(0, 0, 0, 4, 1)
 
 dfs3_ort <- generer_notes_automatique("ORT S3", EC_ORT_S3, evaluations_ORT_S3, poids_ORT_S3, ECTS_ORT_S3)
 
-EC_HUMA_S3 <- c("ANG","C_C","EPS", "LV2")
+EC_HUMA_S3 <- c("ANGL","COMM","EPS", "LV2")
 evaluations_HUMA_S3 <- list(
-  ANG = c("DS2", "CC"),
-  C_C = c("DS2"),
+  ANGL = c("DS2", "CC"),
+  COMM = c("DS2"),
   EPS = c("DS2"),
   LV2 = c("DS2")
 )
 
 poids_HUMAN_S3 <- list(
-  ANG = c(1.5, 1.5),
-  C_C = c(1),
+  ANGL = c(1.5, 1.5),
+  COMM = c(1),
   EPS = c(1),
   LV2 = c(1)
 )
@@ -152,15 +152,15 @@ ECTS_HUMAN_S3 = c(1.5, 1.5, 1, 1)
 dfs3_huma <- generer_notes_automatique("HUMA S3", EC_HUMA_S3, evaluations_HUMA_S3, poids_HUMAN_S3, ECTS_HUMAN_S3)
 
 # EC pour S4
-EC_FONDA_S4 <- c("Geometrie","INFO","PROBA")
+EC_FONDA_S4 <- c("GEOM","INFO","PROBA")
 evaluations_FONDA_S4 <- list(
-  Geometrie = c("DS1", "DS2"),
+  GEOM = c("DS1", "DS2"),
   INFO = c("DS2", "CC"),
   PROBA = c("DS1", "DS2")
 )
 
 poids_FONDA_S4 <- list(
-  Geometrie = c(1.5, 1.5),
+  GEOM = c(1.5, 1.5),
   INFO = c(2, 1),
   PROBA = c(1.5, 1.5)
 )
@@ -169,22 +169,22 @@ ECTS_FONDA_S4 <- c(3.5, 3, 3.5)
 
 dfs4_fonda <- generer_notes_automatique("FONDA S4", EC_FONDA_S4, evaluations_FONDA_S4, poids_FONDA_S4, ECTS_FONDA_S4)
 
-EC_EXP_S4 <- c("Chimie","Electro","Meca","Ondes","TP_Phys","SI")
+EC_EXP_S4 <- c("CHIM","ELMG","MECA","ONDE","PHYS","SI")
 evaluations_EXP_S4 <- list(
-  Chimie = c("DS2", "TP"),
-  Electro = c("DS2", "CC"),
-  Meca = c("DS2", "CC"),
-  Ondes = c("DS2"),
-  TP_Phys = c("TP"),
+  CHIM = c("DS2", "TP"),
+  ELMG = c("DS2", "CC"),
+  MECA = c("DS2", "CC"),
+  ONDE = c("DS2"),
+  PHYS = c("TP"),
   SI = c("TP")
 )
 
 poids_EXP_S4 <- list(
-  Chimie = c(2, 1),
-  Electro = c(2, 1),
-  Meca = c(1, 1),
-  Ondes = c(1),
-  TP_Phys = c(1),
+  CHIM = c(2, 1),
+  ELGM = c(2, 1),
+  MECA = c(1, 1),
+  ONDE = c(1),
+  PHYS = c(1),
   SI = c(1)
 )
 
@@ -194,13 +194,13 @@ dfs4_exp <- generer_notes_automatique("EXP S4", EC_EXP_S4, evaluations_EXP_S4, p
 
 EC_ORT_S4 <- c("PPI","RIE","TSE")
 evaluations_ORT_S4 <- list(
-  PPI = c(),
+  PPI = c("DS2"),
   RIE = c("DS2"),
   TSE = c()
 )
 
 poids_ORT_S4 <- list(
-  PPI = c(),
+  PPI = c(1),
   RIE = c(1),
   TSE = c()
 )
@@ -209,10 +209,10 @@ ECTS_ORT_S4 = c(0, 1, 0)
 
 dfs4_ort <- generer_notes_automatique("ORT S4", EC_ORT_S4, evaluations_ORT_S4, poids_ORT_S4, ECTS_ORT_S4)
 
-EC_HUMA_S4 <- c("ANG","C&C","EPS","LV2")
+EC_HUMA_S4 <- c("ANGL","COMM","EPS","LV2")
 evaluations_HUMA_S4 <- list(
-  ANG = c("DS2", "CC"),
-  C_C = c("DS2"),
+  ANGL = c("DS2", "CC"),
+  COMM = c("DS2"),
   EPS = c("DS2"),
   LV2 = c("DS2")
 )
