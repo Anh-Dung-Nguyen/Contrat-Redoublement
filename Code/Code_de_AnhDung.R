376835d40d91df19344d67d6cf5055c1319947ff
Nguyen Anh Dung <nguyenanhdung2807@gmail.com>
2024-11-22 02:38:03 +0100

    Mettre à jour code pour générer des notes, se concentre également sur la compensation

--- Code/Code_de_AnhDung.R
+++ Code/Code_de_AnhDung.R
@@ -1,28 +1,71 @@
 library(dplyr)
 
-generer_notes_automatique <- function(UE, EC, evaluations){
+generer_notes_automatique <- function(UE, EC, evaluations, poids_evaluation) {
   result_list <- list()
-  
-  for (i in seq_along(EC)){
+
+  for (i in seq_along(EC)) {
     eval_dispo <- evaluations[[EC[i]]]
-    notes <- sapply(eval_dispo, function(x) sample(0:20, 1))
-    moyenne <- mean(notes)
-    validation <- ifelse(moyenne >= 10, "Valide", "Non valide")
-    
-    df_temp <- data.frame(
-      UE = UE,
-      EC = EC[i],
-      Moyenne = moyenne,
-      Validation = validation
-    )
-    
-    df_temp <- cbind(df_temp, t(notes))
-    
+    poids_dispo <- poids_evaluation[[EC[i]]]
+
+    if (length(eval_dispo) > 0){
+      notes <- sapply(eval_dispo, function(x) sample(0:20, 1))
+      moyenne <- sum(notes * poids_dispo) / sum(poids_dispo)
+      validation <- ifelse(moyenne >= 10, "Valide", "Non valide")
+      df_temp <- data.frame(
+        UE = UE,
+        EC = EC[i],
+        Moyenne = moyenne,
+        Validation = validation
+      )
+      df_temp <- cbind(df_temp, t(notes))
+    } else {
+        df_temp <- data.frame(
+          UE = UE,
+          EC = EC[i],
+          Moyenne = NA,
+          Validation = "Non évalué"
+        )
+    }
+
     result_list[[i]] <- df_temp
   }
-  
+
   df_final <- bind_rows(result_list)
   return (df_final)
+}
+
+compensation_intra_UE <- function(df){
+  df <- df %>%
+    group_by(UE) %>%
+    mutate(
+      Moyenne_UE = mean(Moyenne, na.rm = TRUE),
+      Validation = ifelse(Moyenne_UE >= 10 & Validation == "Non valide", "ValideComp", Validation)
+    ) %>%
+    ungroup()
+  return (df)
+}
+
+compensation_inter_UE <- function(df, ue_scientifique){
+  df_scientifique <- df %>% filter(UE %in% ue_scientifique)
+
+  moyenne_ponderee_ue <- df_scientifique %>%
+    group_by(UE) %>%
+    summarise(
+      Moyenne_UE = mean(Moyenne, na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    summarise(
+      Moyenne_globale = mean(Moyenne_UE, na.rm = TRUE)
+    ) %>%
+    pull(Moyenne_globale)
+
+  if(moyenne_ponderee_ue >= 10){
+    df <- df %>%
+      mutate(
+        Validation = ifelse(UE %in% ue_scientifique & Validation == "Non valide", "ValideComp", Validation)
+      )
+  }
+  return (df)
 }
 
 # EC pour S3
@@ -33,7 +76,15 @@ evaluations_FONDA_S3 <- list(
   INFO = c("DS1", "DS2"),
   MECA = c("DS1", "DS2", "CC")
 )
-dfs3_fonda <- generer_notes_automatique("FONDA S3", EC_FONDA_S3, evaluations_FONDA_S3)
+
+poids_FONDA_S3 <- list(
+  ALG = c(1.5, 1.5),
+  ANA = c(1.5, 1.5),
+  INFO = c(1, 1),
+  MECA = c(1, 3, 1)
+)
+
+dfs3_fonda <- generer_notes_automatique("FONDA S3", EC_FONDA_S3, evaluations_FONDA_S3, poids_FONDA_S3)
 
 EC_EXP_S3 <- c("ACSA","Chimie","Electro","TP Phys","Thermo")
 evaluations_EXP_S3 <- list(
@@ -43,7 +94,16 @@ evaluations_EXP_S3 <- list(
   TP_Phys = c("TP"),
   Thermo = c("DS2")
 )
-dfs3_exp <- generer_notes_automatique("EXP S3", EC_EXP_S3, evaluations_EXP_S3)
+
+poids_EXP_S3 <- list(
+  ACSA = c(5, 3),
+  Chimie = c(1, 1, 1),
+  Electro = c(2, 1),
+  TP_Phys = c(2, 1),
+  Thermo = c(1)
+)
+
+dfs3_exp <- generer_notes_automatique("EXP S3", EC_EXP_S3, evaluations_EXP_S3, poids_EXP_S3)
 
 EC_ORT_S3 <- c("ADS","PPI","RIE","Stage","TSE")
 evaluations_ORT_S3 <- list(
@@ -53,15 +113,31 @@ evaluations_ORT_S3 <- list(
   Stage = c("DS2"),
   TSE = c()
 )
-dfs3_ort <- generer_notes_automatique("ORT S3", EC_ORT_S3, evaluations_ORT_S3)
+
+poids_ORT_S3 <- list(
+  ADS = c(),
+  PPI = c(),
+  RIE = c(),
+  Stage = c(1),
+  TSE = c()
+)
+
+dfs3_ort <- generer_notes_automatique("ORT S3", EC_ORT_S3, evaluations_ORT_S3, poids_ORT_S3)
 
 EC_HUMATC_S3 <- c("ANG","C&C","EPS")
 evaluations_HUMATC_S3 <-list(
-  ANG = c("DS2"),
+  ANG = c("DS2", "CC"),
   C_C = c("DS2"),
   EPS = c("DS2")
 )
-dfs3_humatc <- generer_notes_automatique("HUMATC S3", EC_HUMATC_S3, evaluations_HUMATC_S3)
+
+poids_HUMANTC_S3 <- list(
+  ANG = c(1.5, 1.5),
+  C_C = c(1),
+  EPS = c(1)
+)
+
+dfs3_humatc <- generer_notes_automatique("HUMATC S3", EC_HUMATC_S3, evaluations_HUMATC_S3, poids_HUMANTC_S3)
 
 EC_HUMA_S3 <- c(dfs3_humatc$EC, "LV2")
 evaluations_HUMA_S3 <- list(
@@ -70,7 +146,12 @@ evaluations_HUMA_S3 <- list(
   EPS = evaluations_HUMATC_S3$EPS,
   LV2 = c("DS2")
 )
-dfs3_huma <- generer_notes_automatique("HUMA S3", EC_HUMA_S3, evaluations_HUMA_S3)
+
+poids_HUMAN_S3 <- list(
+  LV2 = c(1)
+)
+
+dfs3_huma <- generer_notes_automatique("HUMA S3", EC_HUMA_S3, evaluations_HUMA_S3, poids_HUMAN_S3)
 
 # EC pour S4
 EC_FONDA_S4 <- c("Geometrie","INFO","PROBA")
@@ -79,7 +160,14 @@ evaluations_FONDA_S4 <- list(
   INFO = c("DS2", "CC"),
   PROBA = c("DS1", "DS2")
 )
-dfs4_fonda <- generer_notes_automatique("FONDA S4", EC_FONDA_S4, evaluations_FONDA_S4)
+
+poids_FONDA_S4 <- list(
+  Geometrie = c(1.5, 1.5),
+  INFO = c(2, 1),
+  PROBA = c(1.5, 1.5)
+)
+
+dfs4_fonda <- generer_notes_automatique("FONDA S4", EC_FONDA_S4, evaluations_FONDA_S4, poids_FONDA_S4)
 
 EC_EXP_S4 <- c("Chimie","Electro","Meca","Ondes","TP Phys","SI")
 evaluations_EXP_S4 <- list(
@@ -90,7 +178,17 @@ evaluations_EXP_S4 <- list(
   TP_Phys = c("TP"),
   SI = c("TP")
 )
-dfs4_exp <- generer_notes_automatique("EXP S4", EC_EXP_S4, evaluations_EXP_S4)
+
+poids_EXP_S4 <- list(
+  Chimie = c(2, 1),
+  Electro = c(2, 1),
+  Meca = c(1, 1),
+  Ondes = c(1),
+  TP_Phys = c(1),
+  SI = c(1)
+)
+
+dfs4_exp <- generer_notes_automatique("EXP S4", EC_EXP_S4, evaluations_EXP_S4, poids_EXP_S4)
 
 EC_ORT_S4 <- c("PPI","RIE","TSE")
 evaluations_ORT_S4 <- list(
@@ -98,15 +196,29 @@ evaluations_ORT_S4 <- list(
   RIE = c(),
   TSE = c()
 )
-dfs4_ort <- generer_notes_automatique("ORT S4", EC_ORT_S4, evaluations_ORT_S4)
+
+poids_ORT_S4 <- list(
+  PPI = c(1),
+  RIE = c(),
+  TSE = c()
+)
+
+dfs4_ort <- generer_notes_automatique("ORT S4", EC_ORT_S4, evaluations_ORT_S4, poids_ORT_S4)
 
 EC_HUMATC_S4 <- c("ANG","C&C","EPS")
 evaluations_HUMATC_S4 <-list(
-  ANG = c("DS2"),
+  ANG = c("DS2", "CC"),
   C_C = c("DS2"),
   EPS = c("DS2")
 )
-dfs4_humatc <- generer_notes_automatique("HUMATC S4", EC_HUMATC_S4, evaluations_HUMATC_S4)
+
+poids_HUMANTC_S4 <- list(
+  ANG = c(1.5, 1.5),
+  C_C = c(1),
+  EPS = c(1)
+)
+
+dfs4_humatc <- generer_notes_automatique("HUMATC S4", EC_HUMATC_S4, evaluations_HUMATC_S4, poids_HUMANTC_S4)
 
 EC_HUMA_S4 <- c(dfs4_humatc$EC, "LV2")
 evaluations_HUMA_S4 <- list(
@@ -115,11 +227,22 @@ evaluations_HUMA_S4 <- list(
   EPS = evaluations_HUMATC_S4$EPS,
   LV2 = c("DS2")
 )
-dfs4_huma <- generer_notes_automatique("HUMA S4", EC_HUMA_S4, evaluations_HUMA_S4)
+
+poids_HUMAN_S4 <- list(
+  LV2 = c(1)
+)
+
+dfs4_huma <- generer_notes_automatique("HUMA S4", EC_HUMA_S4, evaluations_HUMA_S4, poids_HUMAN_S4)
 
 # Affichage des dataframes
 dfs3 <- bind_rows(dfs3_fonda, dfs3_exp, dfs3_ort, dfs3_huma)
+dfs3 <- compensation_intra_UE(dfs3)
+dfs3 <- compensation_inter_UE(dfs3, ue_scientifique = c("FONDA S3", "EXP S3"))
+
 dfs4 <- bind_rows(dfs4_fonda, dfs4_exp, dfs4_ort, dfs4_huma)
+dfs4 <- compensation_intra_UE(dfs4)
+dfs4 <- compensation_inter_UE(dfs4, ue_scientifique = c("FONDA S4", "EXP S4"))
+
 df <- bind_rows(dfs3, dfs4)
 
 print (dfs3)
