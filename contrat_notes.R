3961aa91922e4d00222014a273b4d8230374551b
Yaelle-Lamballais <yaelle.lamballais@protonmail.com>
2025-03-06 19:37:15 +0100

    Add files via upload
    ajout constantes MCC, ajout fonctions contrat_notes, pb avec les merge_at

--- contrat_notes.R
+++ contrat_notes.R
@@ -7,57 +7,52 @@ library(dplyr)
 library(flextable)
 library(readxl)
 
+# renvoie les notes de l'élève nom prenom du semestre associé à la feuille de l'excel fichier
+notes_from_jury <- function(fichier, feuille, colonnes, nom, prenom){
+  semestre <- read_excel(fichier, sheet = feuille, col_types = "text")
+  notes <- semestre[semestre[[1]] == nom & semestre[[2]] == prenom, colonnes]
+  return(unlist(notes[rowSums(is.na(notes)) != ncol(notes), ]))
+}
+
+# renvoie un vecteur contenant des croix si l'ec n'est pas validé
+croix <- function(notes){
+  croix <- c()
+  i <- 1
+  for (note in notes){
+    if (as.integer(note) < 10 || is.na(as.integer(note))){ #il faut aussi gérer si c'est en validation ms jsp comment c'est écrit dans le tableur
+      croix[i] <- "X"
+    }else{
+      croix[i] <- ""
+    }
+    i = i+1
+  }
+  return(croix)
+}
+
+
 # création d'un nouvel objet de document Word
 doc <- read_docx()
 
-
-#récupération du fichier de jury avec les moyennes de tous les élèves pour chaque EC
+# récupération du fichier jury
 fichier_jury <- "~/Bureau/3A/EP_perso/jury.xlsx"
 
-#pour lire les données des semestres : feuilles 2 et 3 du fichier exel
-toutes_notes_S3 <- read_excel(fichier_jury, sheet = 2, col_types = "text")
-toutes_notes_S4 <- read_excel(fichier_jury, sheet = 3, col_types = "text")
-
-#pour récupérer la ligne correspondant à l'élève dont on veut créer le contrat
-notes_S3 <- toutes_notes_S3[toutes_notes_S3[[1]] == "Nom1" & toutes_notes_S3[[2]] == "Prenom1", c(6:10, 14:17, 21:25, 29:32)]
-notes_S4 <- toutes_notes_S4[toutes_notes_S4[[1]] == "Nom1" & toutes_notes_S4[[2]] == "Prenom1", c(6:11, 15:17, 21:23, 27:30)]
-
-#pour extraire les valeurs des notes
-valeurs_notes_S3 <- unlist(notes_S3[rowSums(is.na(notes_S3)) != ncol(notes_S3), ])
-valeurs_notes_S4 <- unlist(notes_S4[rowSums(is.na(notes_S4)) != ncol(notes_S4), ])
+# récupération des notes de l'étudiant·e
+notes_S3 <- notes_from_jury(fichier_jury, 2, col_S3, "Nom1", "Prenom1")
+notes_S4 <- notes_from_jury(fichier_jury, 3, col_S4, "Nom1", "Prenom1")
 
 #pour mettre des croix si l'EC n'est pas validé ou n'a pas de note
-croix_S3 <- c()
-i <- 1
-for (note in valeurs_notes_S3){
-  if (as.integer(note) < 10 || is.na(as.integer(note))){
-    croix_S3[i] <- "X"
-  }else{
-    croix_S3[i] <- ""
-  }
-  i = i+1
-}
-
-croix_S4 <- c()
-i <- 1
-for (note in valeurs_notes_S4){
-  if (as.integer(note) < 10 || is.na(as.integer(note))){
-    croix_S4[i] <- "X"
-  }else{
-    croix_S4[i] <- ""
-  }
-  i = i+1
-}
+croix_S3 <- croix(notes_S3)
+croix_S4 <- croix(notes_S4)
 
 # création des tableaux de données
 data <- data.frame(
   LesUE[1],
   LesUE[2],
   LesUE[3],
-  Moyennes = c(valeurs_notes_S3,valeurs_notes_S4),
-  EcValRepasse = c(rep("",34)),
+  Moyennes = c(notes_S3,notes_S4),
+  EcValRepasse = c(rep("",length(LesUE[1]))),
   EcAVal = c(croix_S3, croix_S4)
-  )
+)
 
 signature <- data.frame(
   c1 = c("Date :\n\n\n\n\n"),
@@ -65,10 +60,10 @@ signature <- data.frame(
   c3 = c("\n\n\n\n\n ")
 )
 
-
 #création des flextable
 ft <- flextable(data)
 sign <- flextable(signature)
+
 
 
 #flextable avec la signature à la fin du document word
@@ -89,7 +84,7 @@ sign <- sign %>%
 ft <- ft %>%
   bold(part = "header")%>%
   
-  #pour changer les noms des colonnes
+  #p changement des noms des colonnes
   set_header_labels(
     Ue = "UE (code UE)",
     Ec = "EC",
@@ -99,38 +94,45 @@ ft <- ft %>%
     EcAVal = "EC à valider en 2024-2025"
   )%>%
   
-  #pour concaténer plusieurs lignes et avoir 1 case par UE
-  merge_at(i = 1:length(lesEC_EXPS3), j = 1) %>%
-  merge_at(i = length(lesEC_EXPS3)+1:length(lesEC_FONDAS3), j = 1) %>%
-  merge_at(i = 10:13, j = 1) %>%     #c'est très long d'écrire la longueur à chaque fois, comment faire?
-  merge_at(i = 14:17, j = 1) %>%
-  merge_at(i = 19:21, j = 1) %>%
-  merge_at(i = 22:27, j = 1) %>%
-  merge_at(i = 28:30, j = 1) %>%
-  merge_at(i = 31:34, j = 1) %>%
+  # concaténation de lignes pour avoir 1 case par UE
+  merge_at(i = 1:nb_EC_UE_S3[1], j = 1) %>%
+  merge_at(i = nb_EC_UE_S3[1]+1:nb_EC_UE_S3[1], j = 1) %>%
+  # je comprends pas : 
+# > nb_EC_UE_S3[1]+1:nb_EC_UE_S3[2]
+#   [1]  6  7  8  9 10 11 12 13 14
+# > nb_EC_UE_S3[1]+1:nb_EC_UE_S3[1]
+#   [1]  6  7  8  9 10
   
-  #pour choisir la largeur des colonnes
+  #merge_at(i = nb_EC_UE_S3[2]+1:nb_EC_UE_S3[3], j = 1) %>%
+  # merge_at(i = nb_EC_UE_S3[3]+1:nb_EC_UE_S3[4], j = 1) %>%
+  # 
+  # merge_at(i = nb_EC_UE_S3[5]+1:nb_EC_UE_S4[1], j = 1) %>%
+  # merge_at(i = nb_EC_UE_S4[1]+1:nb_EC_UE_S4[2], j = 1) %>%
+  # merge_at(i = nb_EC_UE_S4[2]+1:nb_EC_UE_S4[3], j = 1) %>%
+  # merge_at(i = nb_EC_UE_S4[3]+1:nb_EC_UE_S4[4], j = 1) %>%
+
+  # choix de la largeur des colonnes
   width(j = c(1,2,3,4,5), width = c(1.5,1.7,1.5,1,1))%>%
   
-  #pour mettre les bords en gras
+  # pour mettre les bords en gras
   border_outer(border = fp_border(color = "black", width = 1, style = "solid"))%>%
   
-  #pour mettre les lignes horizontales et verticales en gris pour pouvoir les visialiser sur document imprimé
+  # pour mettre les lignes horizontales et verticales en gris pour pouvoir les visialiser sur document imprimé
   border_inner_h(border = fp_border(color = "grey", width = 0.5, style = "solid")) %>%
   border_inner_v(border = fp_border(color = "grey", width = 0.5, style = "solid")) %>%
   
-  #pour mettre la ligne horizontale qui sépare le S3 du S4 en gras
+  # pour mettre la ligne horizontale qui sépare le S3 du S4 en gras
   hline(i = nb_EC_S3, border = fp_border(width = 1.5, color = "black"))
   
 
-#pour choisir la police, couleur, taille du titre
+# choix de la police, couleur, taille du titre
 titre <- fpar(
   ftext("CONTRAT D'ÉTUDES 2 STPI", fp_text(font.size = 12, bold = TRUE, font.family = "Arial", color = "cornflowerblue")),
 fp_p = fp_par(text.align = "center")
   )
 
 
-#pour remplir le document Word avec du texte et les flextables
+# pour remplir le document Word avec du texte et les flextables
 doc <- doc %>% 
   body_add_par("") %>%
   body_add_fpar(titre) %>%
@@ -150,5 +152,5 @@ doc <- doc %>%
   body_add_flextable(value = sign)
 
 
-#pour sauvegarder le document
-print(doc, target = "contrat_notes_9_fev.docx")
+# pour sauvegarder le document
+print(doc, target = "~/Bureau/3A/EP_perso/contrat_notes_6_mars.docx")
