84d9aae91e1e8b1dd94afbe1f9b992aa8dad1c38
mvallinb <mvallinb@insa-rennes.fr>
2025-03-29 12:07:22 +0100

    Add files via upload

--- global.R
+++ global.R
@@ -3,12 +3,16 @@ library(ggplot2)
 library(dplyr)
 library(readxl)
 
+lesRedoublants <- data.frame(Nom = character(),
+                 Prénom = character(),
+                 stringsAsFactors = FALSE)
 lesContratsEt<- c()
 lesNomsEt<-c()
 
 # Charger les fichiers ui et server
 source("ui.R")
 source("server.R")
+source("contrat_notes.R")
 
 # Lancer l'application Shiny
 shinyApp(ui = ui, server = server)
